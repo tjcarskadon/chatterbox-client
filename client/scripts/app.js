c37f461346651fc33a028526c87fd46996dbad1b
tjcarskadon <tjcarskadon@gmail.com>
2016-05-02 21:05:48 -0700

    implement app.send

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,8 @@
-// var message = {
-//   username: window.location.search.slice(10),
-//   text: '',
-//   roomname: 'in your head'
-// };
+var message = {
+  username: window.location.search.slice(10),
+  text: '',
+  roomname: 'in your head'
+};
 
 //psuedoclassical method for making the app
 var AppMaker = function() {};
@@ -20,7 +20,7 @@ app.send = function(message) {
 
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: app.server,
+    url: 'https://api.parse.com/1/classes/messages',
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
@@ -57,7 +57,6 @@ app.writeMessages = function(array) {
   for (var i = 0; i < array.results.length; i++) {
     $('.chat-container').append('<div class="message" id=' + array.results[i].objectId + '></div>');
     $('#' + array.results[i].objectId).text(array.results[i].text);
-    console.log(array.results[i]);
   }
 };
 
@@ -66,6 +65,13 @@ app.writeMessages = function(array) {
 $(document).ready(function() {
   //click handler to fetch
   $('#get-messages').on('click', app.fetch);
+  //click handler to send
+  $('#send-messages').on('click', function() { 
+    //get the value 'hihi' from the input field
+    message.text = _.escape($('#input-message').val());
+    $('#input-message').val('')
+    app.send(message); 
+  });
 
 
 
